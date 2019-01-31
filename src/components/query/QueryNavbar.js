ab50466f5c149a252edcd580e630eb8f0091be0c
Rodrigo Machado <souza.machado.rodrigo@gmail.com>
2019-01-31 10:34:37 -0200

    feat(navbar): make RestQL logo clickable

--- src/components/query/QueryNavbar.js
+++ src/components/query/QueryNavbar.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ReactSVG from "react-svg";
+import { Link } from "react-router-dom";
 
-import {
-  Navbar,
-  Button,
-  FormGroup
-} from 'react-bootstrap';
+import { Navbar, Button, FormGroup } from "react-bootstrap";
 
 export default class QueryNavbar extends Component {
-
   render() {
     return (
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
-            <object data={this.props.logo} type="image/svg+xml">
-              <img src={this.props.logo} alt="Logo" />
-            </object>
+            <Link to={"/"}>
+              <ReactSVG src={this.props.logo} alt="Logo" />
+            </Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Navbar.Form pullRight>
             <FormGroup controlId="formInlineName">
-              <Button bsStyle="default"
-                onClick={this.props.toggleSidebar}>
+              <Button bsStyle="default" onClick={this.props.toggleSidebar}>
                 Queries
-							</Button>
-              <Button bsStyle="danger" onClick={this.props.newQuery}>New Query</Button>
+              </Button>
+              <Button bsStyle="danger" onClick={this.props.newQuery}>
+                New Query
+              </Button>
             </FormGroup>
           </Navbar.Form>
         </Navbar.Collapse>
--- src/components/resources/ResourcesNavbar.js
+++ src/components/resources/ResourcesNavbar.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ReactSVG from "react-svg";
+import { Link } from "react-router-dom";
 
-import { Link } from 'react-router-dom';
-
-import {
-  Navbar,
-  FormGroup
-} from 'react-bootstrap';
+import { Navbar, FormGroup } from "react-bootstrap";
 
 export default class ResourcesNavbar extends Component {
-
   render() {
     return (
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
-            <object data={this.props.logo} type="image/svg+xml">
-              <img src={this.props.logo} alt="Logo" />
-            </object>
+            <Link to={"/"}>
+              <ReactSVG src={this.props.logo} alt="Logo" />
+            </Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Navbar.Form pullRight>
             <FormGroup controlId="formInlineName">
-              <Link to={this.props.queryEditorLink} className="btn btn-md btn-info">
+              <Link
+                to={this.props.queryEditorLink}
+                className="btn btn-md btn-info"
+              >
                 Query Editor
-                            </Link>
+              </Link>
             </FormGroup>
           </Navbar.Form>
         </Navbar.Collapse>
