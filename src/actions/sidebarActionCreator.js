4c96b52eff4b6a8bfef9fe52554821848ede6679
Rodrigo Machado <souza.machado.rodrigo@gmail.com>
2020-01-08 18:12:14 -0300

    feat(sidebar): sort query names alphabetically in sidebar (#13)
    * feat(sidebar): sort query names alphabetically in sidebar
    
    * fix(sidebar): prevent error if namespace has no queries

--- src/actions/sidebarActionCreator.js
+++ src/actions/sidebarActionCreator.js
@@ -1,4 +1,3 @@
-
 // Redux actions
 import { SIDEBAR_ACTIONS } from "../reducers/sidebarReducer";
 import { loadQueries, loadRevisionByUrl } from "../api/restQLAPI";
@@ -6,25 +5,29 @@ import { loadQueries, loadRevisionByUrl } from "../api/restQLAPI";
 const store = require("../store/storeConfig").store;
 
 export function handleSidebarLoadQueries(namespace) {
-    const dispatch = store.dispatch;
-  
-    dispatch({
-      type: SIDEBAR_ACTIONS.QUERIES_LOADING,
-      value: namespace
-    });
-  
-    loadQueries(namespace, (response, error) => {
-      if (error) {
-        dispatch({ type: SIDEBAR_ACTIONS.QUERIES_LOADED, value: [] });
-        alert("Error loading queries: " + error);
-      } else {
-        dispatch({
-          type: SIDEBAR_ACTIONS.QUERIES_LOADED,
-          value: response.queries
-        });
-      }
-    });
-  }
+  const dispatch = store.dispatch;
+
+  dispatch({
+    type: SIDEBAR_ACTIONS.QUERIES_LOADING,
+    value: namespace
+  });
+
+  loadQueries(namespace, (response, error) => {
+    if (error) {
+      dispatch({ type: SIDEBAR_ACTIONS.QUERIES_LOADED, value: [] });
+      alert("Error loading queries: " + error);
+    } else {
+      dispatch({
+        type: SIDEBAR_ACTIONS.QUERIES_LOADED,
+        value:
+          response.queries &&
+          response.queries.sort(function(a, b) {
+            return a.id.toLowerCase().localeCompare(b.id.toLowerCase());
+          })
+      });
+    }
+  });
+}
 
 export function handleSidebarLoadQuery(query) {
   const dispatch = store.dispatch;
@@ -52,4 +55,3 @@ export function handleSidebarLoadQuery(query) {
     }
   });
 }
-  
