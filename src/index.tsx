8cfe4658b226ce18234683c289de726ddabaebdc
Caio Ferreira <caiorcferreira@gmail.com>
2021-02-23 11:45:29 -0300

    chore: install react modal

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactModal from 'react-modal';
 import App from './App';
 
 import 'normalize.css';
+
+ReactModal.setAppElement('#modal');
 
 ReactDOM.render(
   <React.StrictMode>
